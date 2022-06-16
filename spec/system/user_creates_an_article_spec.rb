8c67fb6579264fd2f6d0bef4d73699a0337d3a48
Ed SSemuwemba <esseme@gmail.com>
2022-06-16 19:39:02 +0300

    Add article creation
    This feature is to enable this functionality for a user: when I have
    something to say, I want to put it on the internet so that everybody
    can see it
    
    Co-authored-by: Rob Whittaker <rob@thoughtbot.com>

---
+++ spec/system/user_creates_an_article_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "User creates an article" do
+  it "shows their amazing content" do
+    visit root_path
+    fill_in "Title", with: "This is my awesome title"
+    fill_in "Content", with: "My awesome content"
+    click_button "Submit"
+
+    expect(page).to have_content "Article was successfully created"
+    article = Article.last
+    expect(page).to have_content "Article ##{article.id}"
+    expect(page).to have_content article.title
+    expect(page).to have_content article.content
+  end
+
+  context "user provides wrong form data" do
+    it "shows an error on the form" do
+      visit root_path
+
+      click_button "Submit"
+
+      expect(page).to have_content "New article"
+      expect(page).to have_content "2 errors prohibited this article from being saved"
+      expect(page).to have_content "Title can't be blank"
+      expect(page).to have_content "Content can't be blank"
+    end
+  end
+
+end
