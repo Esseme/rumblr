906c216dc4e0dd4ff687f1709b3061cc6bb68d93
Ed SSemuwemba <esseme@gmail.com>
2022-06-16 19:39:02 +0300

    Add create article system test

---
+++ spec/system/user_creates_an_article_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "User creates an article" do
+  it "shows their amazing content" do
+    #As a user
+    #When I visit the home page
+    visit root_path
+    #And I fill in my title and content
+    #And I click "Submit"
+    #Then I should see the title on the page 
+    #And it should show my content
+  end
+end
