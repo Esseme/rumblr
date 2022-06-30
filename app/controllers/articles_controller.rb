e4470b44bcd103690c84ae1c14a73dbac5b1b686
Ed SSemuwemba <esseme@gmail.com>
2022-06-30 09:39:38 +0300

    Make title viewable by user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,21 @@
 class ArticlesController < ApplicationController
   def new
-
+    @article = Article.new
   end
 
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article, notice: "Article was successfully created."
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title)
+  end
 end
--- spec/system/user_creates_an_article_spec.rb
+++ spec/system/user_creates_an_article_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "User creates an article" do
   it "shows their amazing content" do
-    #As a user
-    #When I visit the home page
+    title = "This is my awesome title"
+    content = "My awesome content"
     visit root_path
-    #And I fill in my title and content
-    #And I click "Submit"
-    #Then I should see the title on the page 
-    #And it should show my content
+    fill_in "Title", with: title
+    fill_in "Content", with: content
+    click_button "Submit"
+
+    expect(page).to have_content title
   end
 end
