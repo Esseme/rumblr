e48fa1de889047589f962776100ad19ddf9af95a
Ed SSemuwemba <esseme@gmail.com>
2022-07-07 18:33:51 +0300

    Make form look better

--- spec/system/user_creates_an_article_spec.rb
+++ spec/system/user_creates_an_article_spec.rb
@@ -2,14 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "User creates an article" do
   it "shows their amazing content" do
-    title = "This is my awesome title"
-    content = "My awesome content"
     visit root_path
-    fill_in "Title", with: title
-    fill_in "Content", with: content
+    fill_in "Title", with: "This is my awesome title"
+    fill_in "Content", with: "My awesome content"
     click_button "Submit"
 
-    expect(page).to have_content title
-    expect(page).to have_content content
+    expect(page).to have_content "This is my awesome title"
+    expect(page).to have_content "My awesome content"
   end
 end
