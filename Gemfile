0906d31d410743fdb77ff565aac2b43b7d87f233
Ed SSemuwemba <esseme@gmail.com>
2022-07-11 14:55:06 +0300

    Add tailwindcss

--- Gemfile
+++ Gemfile
@@ -25,3 +25,5 @@ group :test do
   gem "byebug"
   gem "webdrivers"
 end
+
+gem "tailwindcss-rails", "~> 2.0"
--- spec/system/user_creates_an_article_spec.rb
+++ spec/system/user_creates_an_article_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe "User creates an article" do
     expect(page).to have_content "This is my awesome title"
     expect(page).to have_content "My awesome content"
   end
+
+  context "user provides wrong form data" do
+    it "shows an error on the form" do
+    visit root_path
+    fill_in "Title", with: ""
+    fill_in "Content", with: ""
+    click_button "Submit"
+
+    expect(page).to have_content "This is my awesome title"
+    expect(page).to have_content "My awesome content"
+    end
+  end
 end
