22fa4359bd99398c80672c2e0b4cbcef9a8e954e
Ed SSemuwemba <esseme@gmail.com>
2022-11-11 18:55:56 +0300

    Set mailer to use https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = {
-    host: ENV.fetch("APPLICATION_HOST")
+    host: ENV.fetch("APPLICATION_HOST"),
+    protocol: "https"
   }
 
   config.action_mailer.delivery_method = :smtp
