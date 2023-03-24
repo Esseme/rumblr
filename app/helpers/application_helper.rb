6661dcd12770c72ae63750a75e1987b994fbca36
Ed SSemuwemba <esseme@gmail.com>
2023-03-24 17:08:22 +0300

    🎨 Add validation errors on the sign up page
    When a user attempts to sign up without providing email and password,
    there were no validation errors displayed to notify them of the issue.
    
    We have made changes to the UI so the user can get to know what went
    wrong.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def field_with_errors(object, field)
+    if object.errors[field].any?
+      error_messages = object.errors.full_messages_for(field).join(", ")
+      content_tag :div, error_messages, class: "form_error"
+    end
+  end
 end
