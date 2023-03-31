6f29677cfa0d0888f6a5f19eb6d72fb99aee179b
Ed SSemuwemba <esseme@gmail.com>
2023-03-31 15:08:47 +0300

    ♻️  Refactor application helper spec
    Before, we had the "&#39;" character which is a representation
    of the apostrophe but the test was a bit hacky.
    
    We have changed the test to only check for these;
    1. The presence of two errors
    2. The presence of one error
    3. What happens when there is no error
    
    Co-authored-by: Rob Whittaker<rob@thoughtbot.com>

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,24 +1,33 @@
 require "rails_helper"
 
 RSpec.describe ApplicationHelper, "#field_with_error" do
+  it "there are no errors" do
+    user = create(:user)
+
+    field_error = helper.field_with_errors(user, :email)
+
+    expect(field_error).to be_nil
+  end
+
   context "when the field has an error" do
     it "returns div with email error" do
       user = create(:user)
-      user.errors.add(:email, "can't be blank")
       user.errors.add(:email, "is invalid")
-      field_error = helper.field_with_errors(user, :email).tr('"', "'")
-      result =
-        "<div class='form_error'>Email can&#39;t be blank, Email is invalid</div>"
+      user.errors.add(:email, "is too long")
 
+      field_error = helper.field_with_errors(user, :email)
+
+      result = '<div class="form_error">Email is invalid, Email is too long</div>'
       expect(field_error).to eq result
     end
 
     it "returns div with password error" do
       user = create(:user)
-      user.errors.add(:password, "can't be blank")
-      field_error = helper.field_with_errors(user, :password).tr('"', "'")
-      result = "<div class='form_error'>Password can&#39;t be blank</div>"
+      user.errors.add(:password, "cannot be blank")
 
+      field_error = helper.field_with_errors(user, :password)
+
+      result = '<div class="form_error">Password cannot be blank</div>'
       expect(field_error).to eq result
     end
   end
