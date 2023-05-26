983cff22a288e770e7fde827d6536862b01b253b
Ed SSemuwemba <esseme@gmail.com>
2023-05-26 13:35:12 +0300

    ✨ Show flash when article is unedited
    Show a message to the user when they visit the edit page but not make
    any changes but click the submit button.

--- spec/system/user_edits_an_article_spec.rb
+++ spec/system/user_edits_an_article_spec.rb
@@ -3,11 +3,15 @@ require "rails_helper"
 RSpec.describe "User edits an article" do
   it "shows an edit button on the article show page" do
     user = create(:user)
+    article = create(
+      :article,
+      content: "My awesome content",
+      title: "This is my awesome title",
+      user: user
+    )
+
     visit root_path(as: user)
-    fill_in "Title", with: "This is my awesome title"
-    fill_in "Content", with: "My awesome content"
     click_button "Submit"
-    article = Article.last
     visit article_path(as: user, id: article.id)
 
     expect(page).to have_selector(:link_or_button, "Edit")
@@ -16,20 +20,38 @@ RSpec.describe "User edits an article" do
   context "user clicks edit button" do
     it "shows the edited article" do
       user = create(:user)
-      visit root_path(as: user)
-      fill_in "Title", with: "This is my awesome title"
-      fill_in "Content", with: "My awesome content"
-      click_button "Submit"
-      article = Article.last
+      article = create(
+        :article,
+        content: "My awesome content",
+        title: "This is my awesome title",
+        user: user
+      )
+
       visit edit_article_path(as: user, id: article.id)
       fill_in "Title", with: "This is my edited awesome title"
       fill_in "Content", with: "My edited awesome content"
       click_button "Submit"
 
       expect(page).to have_content "Article was successfully edited"
-      edited_article = Article.last
-      expect(page).to have_content edited_article.title
-      expect(page).to have_content edited_article.content
+      expect(page).to have_content "This is my edited awesome title"
+      expect(page).to have_content "My edited awesome content"
+    end
+
+    it "shows the unedited article with a flash" do
+      user = create(:user)
+      article = create(
+        :article,
+        content: "My wawsome content",
+        title: "This is my awesome title",
+        user: user
+      )
+
+      visit edit_article_path(article, as: user)
+      click_button "Submit"
+
+      expect(page).to have_content "No changes were made to the article"
+      expect(page).to have_content "This is my awesome title"
+      expect(page).to have_content "My wawsome content"
     end
   end
 end
