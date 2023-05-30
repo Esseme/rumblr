8c2da7a783ace9dd0978551529649c0fb5765834
Ed SSemuwemba <esseme@gmail.com>
2023-05-30 12:58:24 +0300

    ♻️  Add a previously_changed? method
    This was done to improve the readability of the code and consequently
    make it easy to maintain

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :content
   belongs_to :user
+
+  def previously_changed?
+    previous_changes.empty?
+  end
 end
