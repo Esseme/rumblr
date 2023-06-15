516ae852723338890e287ee7b0049a02f738a090
Ed SSemuwemba <esseme@gmail.com>
2023-06-15 12:39:54 +0300

    ✨ Add an admin dashboard
    We have added an admin dashboard so that the admin user can easily
    moderate the site.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :articles
+    resources :users
+
+    root to: "articles#index"
+  end
   resources :articles
   resources :users
   root "articles#new"
