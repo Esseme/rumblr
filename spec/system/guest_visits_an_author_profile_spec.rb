1ab6d2e4a73664f808241f9ca36705ce68b82f35
Ed SSemuwemba <esseme@gmail.com>
2023-10-27 15:43:19 +0300

    Allow guest to view articles created by a user
    Before, a guest wouldn't be able to view articles created by a user.
    Now, a guest can simply navigate to any user's profile and they will
    view the articles created by that user.
    
    Co-authored-by: Rob Whittaker <rob@thoughbot.com>

---
+++ spec/system/guest_visits_an_author_profile_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe "Guest visits an author profile" do
+  it "displays a list of articles created by that author" do
+    author = create(:user)
+
+    create :article, user: author, title: "TEST_ARTICLE_1"
+    create :article, user: author, title: "TEST_ARTICLE_2"
+    create :article, title: "TEST_ARTICLE_3"
+
+    visit user_path(author)
+
+    expect(page).to have_content "TEST_ARTICLE_1"
+    expect(page).to have_content "TEST_ARTICLE_2"
+    expect(page).not_to have_content "TEST_ARTICLE_3"
+  end
+end
