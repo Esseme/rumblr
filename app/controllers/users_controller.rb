549c54ce57ea0e73c8d6be2f9014d00526ecc965
Ed SSemuwemba <esseme@gmail.com>
2023-10-27 15:43:19 +0300

    Allow guest to view articles created by a user
    Before, a guest wouldn't be able to view articles created by a user.
    Now, a guest can simply navigate to any user's profile and they will
    view the articles created by that user.
    
    Co-authored-by: Rob Whittaker <rob@thoughbot.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < Clearance::UsersController
+  skip_before_action :require_login, only: [:create, :new, :show], raise: false
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
